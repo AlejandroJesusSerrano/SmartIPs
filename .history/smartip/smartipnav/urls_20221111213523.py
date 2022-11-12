9546d0392aab1cb4806ab301bb7b8fece8cf1b08
AlejandroJesusSerrano <alejandrojserrano@hotmail.com>
2022-11-12 19:01:08 -0300

    template concluido y herencia

--- .history/smartip/smartipnav/urls_20221111213523.py
+++ .history/smartip/smartipnav/urls_20221111213523.py
@@ -2,7 +2,7 @@ from django.urls import path
 from smartipnav import views
 
 urlpatterns = [
-    path('index', views.index, name="Inicio"),
+    path('index', views.index),
     path('printers', views.printers, name="Printers"),
     path('computers', views.computer, name="Computers"),
     path('ip', views.ip, name="Ip"),
--- .history/smartip/smartipnav/urls_20221112141753.py
+++ .history/smartip/smartipnav/urls_20221112141753.py
@@ -2,9 +2,9 @@ from django.urls import path
 from smartipnav import views
 
 urlpatterns = [
-    path('index', views.index, name="Inicio"),
+    path('index', views.index),
     path('printers', views.printers, name="Printers"),
-    path('computers', views.computer, name="Computers"),
+    path('computers', views.computers, name="Computers"),
     path('ip', views.ip, name="Ip"),
     path('userspc', views.userspc, name="UsersPc"),
     path('offices', views.office, name="Offices"),
